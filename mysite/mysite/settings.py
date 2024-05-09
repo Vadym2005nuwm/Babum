eabc263b1586bdf91cc246cd8bd827bc447f7d6c
Vadym2005nuwm <kokhanets_ak22@nuwm.edu.ua>
2024-05-09 16:01:04 +0300

    Були стоврені Django-аплікації та проведена робота по підключенню бази даннихта інтерфейсом адміністратора.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'mysite_usr',
+        'PASSWORD': 'mysite_pass',
+
     }
 }
 
