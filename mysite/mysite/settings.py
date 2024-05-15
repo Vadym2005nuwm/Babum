5e1fb4e2f4d94b3b6aec799161361a870f0cb5f2
Vadym2005nuwm <kokhanets_ak22@nuwm.edu.ua>
2024-05-15 12:26:55 +0300

    Трішки намудрив тому переобив.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,8 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'mysite_usr',
-        'PASSWORD': 'mysite_pass',
+
 
     }
 }
@@ -119,9 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
